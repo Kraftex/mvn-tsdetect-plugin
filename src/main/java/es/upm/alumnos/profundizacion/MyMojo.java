354ac5135a83ae6cbaa477042e273062e73e9c70
Julián C <julian.gcastro@alumnos.upm.es>
2024-10-27 01:39:30 +0200

    Adding execution of the JAR file, new properties and tepmplate functions to be finished.

--- src/main/java/es/upm/alumnos/profundizacion/MyMojo.java
+++ src/main/java/es/upm/alumnos/profundizacion/MyMojo.java
@@ -9,6 +9,8 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +28,42 @@ public class MyMojo extends AbstractMojo
 {
 	private static final String JAVA_EXT = ".java";
 	private static final Predicate<String> isJavaFile = file -> file.endsWith(JAVA_EXT);
+	public class ProcessHolder
+	{
+		/*public static ProcessHolder from ( ProcessBuilder builder )
+		{
+			return new ProcessHolder(builder.start());
+		}*/
+		
+		private Process proc;
+		
+		public ProcessHolder ( Process proc )
+		{
+			this.proc = proc;
+			info("PID[%d]", proc.pid());
+		}
+		
+		public ProcessHolder waitFor ( ) throws InterruptedException
+		{
+			proc.waitFor();
+			return this;
+		}
+		
+		public void printOutput ( )
+		{
+			InputStream out = proc.getInputStream();
+			byte b[];
+			try {
+				b = new byte[out.available()];
+				out.read(b,0,b.length);
+				info(new String(b));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	public static class SuperObject
 	{
 		@Override
@@ -106,6 +144,9 @@ public class MyMojo extends AbstractMojo
     @Parameter(defaultValue = "${project.build.testSourceDirectory}")
     private File testCodeDir;
     
+    @Parameter(defaultValue = "${project.reporting.outputDirectory}")
+    private File projReportDir;
+    
     @Parameter(defaultValue = "${project.groupId}")
     private String projGroupId;
     
@@ -113,8 +154,15 @@ public class MyMojo extends AbstractMojo
     private String projArtifactId;
     
     //configuration has higher preference over pom project property.
-    @Parameter(defaultValue = "<blank>", property = "tsdetect.inputCSV", required = false)
-    private String inputCSV;
+    @Parameter(property = "tsdetect.jar", required = true)
+    private File jar;
+    
+    //TODO: Make debugs to be output as infos
+    @Parameter(property = "tsdetect.verbose")
+    private boolean verbose = false;
+    
+    @Parameter(defaultValue = "java", property = "tsdetect.java", required = false)
+    private String java;
     
     @Parameter
     private String nothing;
@@ -122,6 +170,7 @@ public class MyMojo extends AbstractMojo
     public void execute() throws MojoExecutionException
     {
     	info("pwd: %s", pwd());
+    	info("java.home: ",System.getProperty("java.home"));
     	Map context = getPluginContext();
     	if (context != null)
     	{
@@ -134,11 +183,22 @@ public class MyMojo extends AbstractMojo
     	info("Auto variables:");
     	printAttribute("sourceCodeDir", "- %s: %s");
     	printAttribute("testCodeDir", "- %s: %s");
+    	printAttribute("projReportDir", "- %s: %s");
     	printAttribute("projGroupId", "- %s: %s");
     	printAttribute("projArtifactId", "- %s: %s");
     	info("Other variables:");
     	printAttribute("nothing", "- %s: %s");
-    	printAttribute("inputCSV", "- %s: %s");
+    	printAttribute("verbose", "- %s: %s");
+    	printAttribute("jar", "- %s: %s");
+    	if (!jar.exists())
+    	{
+    		throw reportException(null, "File '%s' doesn't exist.\nCheck property 'tsdetect.jar' or configuration for 'jar'.", jar.getAbsolutePath());
+    	}
+    	if (!projReportDir.exists())
+    	{
+    		info("Creating reporting directory: %s", projReportDir.getAbsolutePath());
+    		projReportDir.mkdirs();
+    	}
     	info("Gathering production files:");
     	final List<InfoFile> prodFiles = getProductionFiles();
     	prodFiles.forEach( file -> info("- %s", file) );
@@ -148,8 +208,62 @@ public class MyMojo extends AbstractMojo
     	info("Matching prod-test files:");
     	final Map<InfoFile, InfoFile> matchedFiles = matchProductionToTestFile(prodFiles, testFiles);
     	matchedFiles.entrySet().forEach( entry -> info("- %s > %s", entry.getKey().name(), entry.getValue().name()) );
+    	File inputCSV = null;
+    	try {
+			inputCSV = File.createTempFile("inputCSV", null);
+			inputCSV.deleteOnExit();
+			writeInputCSV(inputCSV, matchedFiles);
+		}
+    	catch (IOException e) {
+    		throw reportException(e, "Can't create the input file to run the JAR.\nDirectory: %s", pwd());
+		}
+    	try {
+    		//new ProcessHolder(runCMD("pwd")).waitFor().printOutput();
+			new ProcessHolder(runJAR(inputCSV)).waitFor().printOutput();
+		}
+    	catch (InterruptedException e) {
+    		throw reportException(e, "There was an error running the JAR.");
+		}
+    	
     	//Report errors through MojoExecutionException, wrapping actual Exception
     	//throw reportException(e, "Error creating file %s", filename);
+    }
+    
+    private File getLastOutputReport ( )
+    {
+    	return null;
+    }
+    
+    private Process runCMD ( String... cmd ) throws MojoExecutionException
+    {
+    	ProcessBuilder runner = new ProcessBuilder(cmd);
+    	runner.redirectErrorStream(true);
+		//runner.directory(projReportDir);
+		try {
+			return runner.start();
+		}
+		catch (IOException e) {
+    		throw reportException(e, "Can't run the JAR.");
+		}
+    }
+    
+    private Process runJAR ( File inputCSV ) throws MojoExecutionException
+    {
+    	ProcessBuilder runner = new ProcessBuilder("java", "-jar", jar.getAbsolutePath(), inputCSV.getAbsolutePath());
+    	runner.redirectErrorStream(true);
+		runner.directory(projReportDir);
+		try {
+			return runner.start();
+		}
+		catch (IOException e) {
+    		throw reportException(e, "Can't run the JAR.");
+		}
+    }
+    
+    public void writeInputCSV ( final File inputCSV, final Map<InfoFile, InfoFile> mathedFiles )
+    {
+    	//TODO
+    	//Format to write: appName, testFile, sourceFile
     }
     
     public List<InfoFile> getProductionFiles ( )
